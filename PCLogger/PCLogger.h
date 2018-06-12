bf3eab2198a346e459aa2c312f4067da41f242d1
pc <su.pingchen@linecorp.com>
2018-06-12 21:25:34 +0900

    Initial Commit

---
+++ PCLogger/PCLogger.h
@@ -0,0 +1,19 @@
+//
+//  PCLogger.h
+//  PCLogger
+//
+//  Created by PingChen (PC) on 2018/06/12.
+//  Copyright © 2018 spc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PCLogger.
+FOUNDATION_EXPORT double PCLoggerVersionNumber;
+
+//! Project version string for PCLogger.
+FOUNDATION_EXPORT const unsigned char PCLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PCLogger/PublicHeader.h>
+
+
